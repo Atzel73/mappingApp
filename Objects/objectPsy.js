e83c6124bd14b829144e7b8389a081ac66b7d0ac
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-05 15:03:57 -0600

    se agregaron cartas de estilo a los objetos de prueba

--- Objects/objectPsy.js
+++ Objects/objectPsy.js
@@ -1,36 +1,36 @@
 import { React } from 'react';
 
 
-const masters = [{
+export const masters = [{
     name: "Juan",
-    data: [{
-        photo: "",
-        age: 23,
-        location: "San Juan",
-        street: "Villalobos",
-        profession: "Psychiatric"
-    }]
+
+    photo: "https://i.pinimg.com/564x/05/4a/cd/054acd9d810b1cc4addd9fb0a1cc046d.jpg",
+    age: 23,
+    location: "San Juan",
+    street: "Villalobos",
+    profession: "Psychiatric"
+
 },
 {
-    name: Jose,
-    data: [{
-        photo: "",
-        age: 23,
-        location: "Zona Centro",
-        street: "Zarco #706",
-        profession: "Psychiatric"
-    }]
+    name: "Jose",
+
+    photo: "https://i.pinimg.com/564x/4e/f8/62/4ef862749ec67d006bcbdeb23dfa1f4d.jpg",
+    age: 23,
+    location: "Zona Centro",
+    street: "Zarco #706",
+    profession: "Psychiatric"
+
 },
 {
-    name: Ivan,
-    data: [{
-        photo: "",
-        age: 72,
-        location: "Villas del Guadiana",
-        street: "Orion #305",
-        profession: "Psychiatric"
-    }]
+    name: "Ivan",
+
+    photo: "https://i.pinimg.com/564x/ce/60/45/ce60453c50de808e7383543bc531d9d5.jpg",
+    age: 72,
+    location: "Villas del Guadiana",
+    street: "Orion #305",
+    profession: "Psychiatric"
 }]
+
 
 
 
