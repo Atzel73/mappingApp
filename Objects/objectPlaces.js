303b02c2cee280e975cd82d00e4f368e45924d05
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-07 17:02:04 -0600

    imagenes de tarjetas editadas

--- Objects/objectPlaces.js
+++ Objects/objectPlaces.js
@@ -7,7 +7,7 @@ export const places = [
         latitude: 24.03195663600582, 
         longitude: -104.67376510853963
     },
-    photo: "../assets/places/IDJ.jpeg",
+    photo: "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=TzDU2ZgaTirezME4QZzSVg&cb_client=search.gws-prod.gps&w=408&h=240&yaw=304.3983&pitch=0&thumbfov=100",
     instituto: " Instituto Duranguense de la Juventud ",
     domicilio: " Calle Gomez Palacio #934, entre Hidalgo y Zaragoza. ",
     codigoPostal: 34000, 
@@ -22,7 +22,7 @@ export const places = [
         latitude: -25.436658952319963,
         longitude: -49.28039871579469,
     },
-    photo: "../assets/places/CAIF.jpeg",
+    photo: "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=aPlp5wr1mR-Y5HGpJ9VDPA&cb_client=search.gws-prod.gps&w=408&h=240&yaw=3.1910462&pitch=0&thumbfov=100",
     instituto: " Centro de Psicoterapia Familiar e Individual ",
     domicilio: " Blv. Jose Maria Patoni, Manzana #105, Predio Rustico La Tinaja y Los Lugos. ",
     codigoPostal: 34217,
@@ -37,7 +37,7 @@ export const places = [
         latitude: 23.997449720052934, 
         longitude: -104.67612405898662
     },
-    photo: "../assets/places/CESSA.jpeg",
+    photo: "https://lh3.googleusercontent.com/gps-proxy/ALd4DhEiLP6mcr356wqT0LcSSWRiLk2jKPPLyiedDH37_XJyoD0d_aVuRPXoflm2WXlu77JMbcna_7mRJvJMFoVIqfBoaw3TF0tr1YKNI3abGGAXnVNo6WLmYL_ZB9MIKm1Rzft1S9i8OMCq5ltIev49k8PVCB0RZZmVQe4RhomHVdr_LxIKv3ciFbyjZu5hslEzxIjcdLI=w427-h240-k-no",
     instituto: " CESSA 450 (Centro de Salud con Servicios Ampliados).  ",
     domicilio: " Toma de Zacatecas N. 129, Col. Division Del Norte (Division Del Norte) ",
     codigoPostal: 34000,
@@ -52,7 +52,7 @@ export const places = [
         latitude: 24.029883058669277, 
         longitude: -104.60383768966523
     },
-    photo: "../assets/places/CIJ.jpeg",
+    photo: "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=srca6XM8kIJRftBvrlStPg&cb_client=search.gws-prod.gps&w=408&h=240&yaw=6.7662673&pitch=0&thumbfov=100",
     instituto: " CIJ (Centro de Integracion Juvenil) ",
     domicilio: " Carretera Mexico Km 3.5 Mz. A Lote 8 Col. Industrial ",
     codigoPostal: 34000,
@@ -67,7 +67,7 @@ export const places = [
         latitude: 24.046446919923543, 
         longitude: -104.59385274733452
     },
-    photo: "../assets/places/miguelVallebueno.jpeg",
+    photo: "https://lh5.googleusercontent.com/p/AF1QipMf2HS9LjdD238I8HJ0QXqVv_aJWrL07S_f37qK=w426-h240-k-no",
     instituto: " Hospital de Salud Mental Dr. Miguel Vallebueno ",
     domicilio: " Av. Fidel VElazquez Sanchez s/n, Agricola Veinte de Noviembre ",
     codigoPostal: 34234,
