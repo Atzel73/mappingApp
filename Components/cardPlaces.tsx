2980b5ae026f596ca221d236cdcaed8ef69ddad4
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-07 19:43:50 -0600

    cartas ajustadas

--- Components/cardPlaces.tsx
+++ Components/cardPlaces.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, Card } from '@rneui/themed';
 import { places } from '../Objects/objectPlaces';
+import { ScreenWidth } from '@rneui/base';
 
-
+const CARD_WIDTH = ScreenWidth * 0.9;
+const SPACE_FOR_CARD_INSET = ScreenWidth * 0.1 -10;
 export const  targetPlaces = () => {
     return( 
         <>
         
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      <ScrollView horizontal 
+      scrollEventThrottle={1}
+      showsHorizontalScrollIndicator={false}
+      style={styles.chipView}
+      contentInset={{
+        top:0,
+        left:0,
+        bottom:0,
+        right:0,
+
+      }}
+      snapToInterval={CARD_WIDTH + 20}
+      
+      snapToAlignment='center'
+>
         <View style={styles.container}>
             {places.map(place => {
                 return(
+                  <TouchableOpacity style={styles.chipTouch}>
                   <ScrollView >
                     <Card>
                         <Card.Image
@@ -20,8 +37,6 @@ export const  targetPlaces = () => {
                     uri: place.photo,
                   }}
                 />
-                
-
                 <Card.Title style={styles.infoMax}>{place.instituto}</Card.Title>
                 <Text style={styles.info}> <Text style={styles.infoMax}>Ubicacion: </Text>{place.domicilio}</Text>
                 <Text style={styles.info}> <Text style={styles.infoMax}>Codigo Postal: </Text>{place.codigoPostal.toString()}</Text>
@@ -32,6 +47,7 @@ export const  targetPlaces = () => {
                 <Text style={styles.info}> <Text style={styles.infoMax}>Requisitos para recibir atencion: </Text>{place.requisitosAtencion}</Text>
                     </Card>
                     </ScrollView>
+                    </TouchableOpacity>
                 );
             })}
         </View>
@@ -41,6 +57,18 @@ export const  targetPlaces = () => {
 }
 
 const styles = StyleSheet.create({
+  chipTouch:{
+    flexDirection: 'row',
+    borderRadius:20,
+    padding: 8,
+    paddingHorizontal: 20,
+    marginHorizontal: 10,
+    height: 35,
+  },
+  chipView: {
+    position: 'absolute',
+    paddingHorizontal: 10,  
+    },
     container: {
       flexDirection: 'row',
       padding: 10,
@@ -50,7 +78,7 @@ const styles = StyleSheet.create({
       width: 150, 
     },
     image: {
-      width: '100%',
+      width: '50%',
       height: 150, 
       marginBottom: 8,
     },
