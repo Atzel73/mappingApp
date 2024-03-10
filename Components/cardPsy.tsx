403c66ba0e5bde1b8b4026a6339d0076dabaeaf3
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-10 15:40:05 -0600

    secccion de doctores: terminada

--- Components/cardPsy.tsx
+++ Components/cardPsy.tsx
@@ -6,23 +6,27 @@ import { masters } from '../Objects/objectPsy';
 export const Target = () => {
   return (
     <>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <View style={styles.container}>
+      <ScrollView >
+        <View style={styles.container} >
           {masters.map(master => {
             return (
-              <Card>
-                <Card.Image
-                  style={styles.image}
-                  source={{
-                    uri: master.photo,
-                  }}
-                />
-                <Card.Title>{master.name}</Card.Title>
-                <Text style={styles.info}> Edad: {master.age}</Text>
-                <Text style={styles.info}>Ubicacion: {master.location}</Text>
-                <Text style={styles.info}> Calle: {master.street}</Text>
-                <Text style={styles.info}> Titulo: {master.profession}</Text>
-              </Card>
+              <View style={styles.user} >
+                <Card>
+                  <Card.Image
+                    style={styles.image}
+                    source={{
+                      uri: master.photo,
+                    }}
+                  />
+                  <Card.Title>{master.name}</Card.Title>
+                  <Card.Divider />
+                  <Text style={styles.info}> Edad: {master.age}</Text>
+                  <Text style={styles.info}> Ubicacion: {master.location}</Text>
+                  <Text style={styles.info}> Calle: {master.street}</Text>
+                  <Text style={styles.info}> Titulo: {master.profession}</Text>
+                  <Card.Divider />
+                </Card>
+              </View>
             );
           })}
         </View>
@@ -33,20 +37,28 @@ export const Target = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     padding: 10,
+    backgroundColor: '#E8DAF4' 
   },
+user: {
+  flexDirection: 'column',
+  marginBottom: 6,
+},
   card: {
     marginRight: 10,
-    width: 150, 
+    width: 150,
   },
   image: {
+    alignSelf: 'center',
     width: '100%',
-    height: 150, 
-    marginBottom: 8,
+    height: 100,
+    resizeMode: 'cover',
+    borderRadius: 10,
   },
   info: {
-    fontSize: 14,
+    fontSize: 12,
     marginTop: 4,
+    textAlign: 'justify',
   },
 });
